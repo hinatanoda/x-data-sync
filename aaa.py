909c54caa364e4f0adb08bbcee2f40e426ec1534
hinatanoda <h.noda.king@gmail.com>
2024-11-17 19:35:58 +0900

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -1,9 +1,1 @@
-import tweepy
 
-# Bearer Tokenを使って認証
-BEARER_TOKEN = "your_bearer_token"
-client = tweepy.Client(bearer_token=BEARER_TOKEN)
-
-# 自分のユーザーIDを取得
-user = client.get_me()
-print(user.data.id)  # これがuser_id
